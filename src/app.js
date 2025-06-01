50562a6be06818757aeeabab1344cfcf0af7c78d
arbaazshaikh08 <arbajshaikh@gmail.com>
2025-06-01 09:16:45 +0530

    Add health check endpoint to confirm server status

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Server is live");
+});
+
 import booksRouter from "./routes/books.router.js";
 
 app.use("/api/v1/books", booksRouter);
